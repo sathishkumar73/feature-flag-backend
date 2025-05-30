5dd3fed8d5b3313d773a219d529b3708435cc9bf
sathishkumar73 <sathishkumareceskct@gmail.com>
2025-05-30 19:09:58 +0530

    fix: support for api-key

---
+++ src/api-key/api-key.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ApiKeyController } from './controllers/api-key.controller';
+import { ApiKeyService } from './services/api-key.service';
+
+@Module({
+  providers: [ApiKeyService],
+  controllers: [ApiKeyController]
+})
+export class ApiKeyModule {}
